548c0599d45996193ec3976580c39a5361939743
Marie-Eve Ngoy Bomolo <marieeve.ngoybomolo@student.hogent.be>
2024-05-07 13:09:22 +0200

    changed the api to ElevenlabsAPI instead of ExampleApi

--- src/main.py
+++ src/main.py
@@ -20,12 +20,13 @@ from screens.settings import Settings
 from screens.main_screen import MainScreen
 from modules.dialog.exitdialog import ExitDialog
 from modules.util.widget_loader import load_widget
-from settings.app_settings import GlobalSettings 
+from settings.app_settings import GlobalSettings
 from api.api_factory import load_apis
 
 # FIXME This folder shall always be created in the location of the executable, but currently will always be created in the current working directory
 # NOTE Need to differentiate launch environment (Release vs. Debug) - where one gets executed via python, the other via deployed executable
 TMP_FOLDER = 'tmp'
+
 
 class TrailingPressedIconButton(
     ButtonBehavior, RotateBehavior, MDListItemTrailingIcon
@@ -33,15 +34,20 @@ class TrailingPressedIconButton(
     # NOTE necessary class to allow for the chevron to rotate when the expansion panel is opened or closed
     ...
 
+
 class SpeechJokey(MDApp):
     def build(self):
         # load_widget(os.path.join(os.path.dirname(loaddialog.__file__), 'loaddialog.kv'))
         # load_widget(os.path.join(os.path.dirname(savedialog.__file__), 'savedialog.kv'))
         # load_widget(os.path.join(os.path.dirname(app_settings.__file__), 'AppSettingsPopup.kv'))
-        load_widget(os.path.join(os.path.dirname(sys.modules[MainScreen.__module__].__file__), 'main_screen.kv'))
-        load_widget(os.path.join(os.path.dirname(sys.modules[Settings.__module__].__file__), 'settings.kv'))
-        load_widget(os.path.join(os.path.dirname(sys.modules[About.__module__].__file__), 'about.kv'))
-        load_widget(os.path.join(os.path.dirname(sys.modules[ExitDialog.__module__].__file__), 'exitdialog.kv'))
+        load_widget(os.path.join(os.path.dirname(
+            sys.modules[MainScreen.__module__].__file__), 'main_screen.kv'))
+        load_widget(os.path.join(os.path.dirname(
+            sys.modules[Settings.__module__].__file__), 'settings.kv'))
+        load_widget(os.path.join(os.path.dirname(
+            sys.modules[About.__module__].__file__), 'about.kv'))
+        load_widget(os.path.join(os.path.dirname(
+            sys.modules[ExitDialog.__module__].__file__), 'exitdialog.kv'))
         self.sm = ScreenManager()
         # self.screens = [Screen(name='Title {}'.format(i)) for i in range(4)]
         # self.screens = {
@@ -51,12 +57,13 @@ class SpeechJokey(MDApp):
         # }
         self.global_settings = GlobalSettings()
         self.icon = os.path.join(os.curdir, 'speech-jokey.ico')
-        Config.set('kivy','window_icon', self.icon)
+        Config.set('kivy', 'window_icon', self.icon)
         log.setLevel(LOG_LEVELS["debug"])
         self.apis = load_apis()
-        self.api = self.apis.get("ExampleAPI", None)
+        self.api = self.apis.get("ElevenLabsAPI", None)
         self.sm.add_widget(MainScreen(title="Speech Jokey", name="main"))
-        self.sm.add_widget(Settings(title="Settings", api=self.api, name="settings"))
+        self.sm.add_widget(
+            Settings(title="Settings", api=self.api, name="settings"))
         self.sm.add_widget(About(title="About", name="about"))
         return self.sm
 
@@ -84,6 +91,7 @@ class SpeechJokey(MDApp):
             # panel.parent.height += child.height if not panel.is_open else -child.height
             # panel.parent.parent.height += child.height if not panel.is_open else -child.height
         os.makedirs(TMP_FOLDER, exist_ok=True)
+
 
 if __name__ == '__main__':
     if hasattr(sys, '_MEIPASS'):
