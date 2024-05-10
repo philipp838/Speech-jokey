fcde403369f3f419f0d9f255b854f42810f59aa7
Isabella Reiher <easyreiher@gmail.com>
2024-05-10 17:24:44 +0200

    both ExampleAPI and ElevenlabsAPI appear at settings screen (still does not work to expand expansion panel though)

--- src/screens/settings.py
+++ src/screens/settings.py
@@ -11,12 +11,14 @@ from kivymd.uix.widget import MDWidget
 
 class Settings(MDScreen):
     title = StringProperty()
-    def __init__(self, title: str, api: MDWidget, *args, **kwargs):
+    def __init__(self, title: str, apis: MDWidget, *args, **kwargs):
         super(Settings, self).__init__(*args, **kwargs)
         self.title = title
-        log.debug("%s: API: %s", self.__class__.__name__, api)
-        if api is not None:
-            try:
-                self.ids.settings_container.add_widget(api)
-            except Exception as e:
-                log.error("Error adding widget to settings_container: %s", str(e))
+        log.debug("%s: API: %s", self.__class__.__name__, apis)
+        for api in apis:
+
+            if api is not None:
+                try:
+                    self.ids.settings_container.add_widget(api.settings.get_settings_widget())
+                except Exception as e:
+                    log.error("Error adding widget to settings_container: %s", str(e))
