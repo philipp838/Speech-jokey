371b30a1211c388124177bcaeb18df0c6fad6fbe
Martin Deinhofer <deinhofer.m@gmail.com>
2024-11-28 12:09:39 +0100

    Refactored ApiFactory to singleton and global object. Moved static methods to class methods and add methods for getting and setting active api

--- src/api/api_factory.py
+++ src/api/api_factory.py
@@ -8,41 +8,95 @@ import traceback
 # Custom
 from modules.util.widget_loader import load_widget
 
-
 class ApiFactory:
-    @staticmethod
-    def get_api(api_name: str):
-        try:
-            api_module = importlib.import_module(
-                f"api.{api_name.lower()}.{api_name.lower()}")
-            log.debug("%s: Imported API module: %s",
-                      __class__.__name__, api_module)
-            api_class = getattr(api_module, api_name)
-            settings_class = getattr(api_module, f"{api_name}Settings")
-            load_widget(os.path.join(os.path.dirname(
-                api_module.__file__), f"{api_name.lower()}.kv"))
-            # NOTE THIS is the ONLY place where the API instance is created (And should be created)
-            api_instance = api_class(settings_class(settings_class.get_settings_widget()))
-            log.debug("%s: Created API instance: %s", __class__.__name__, api_instance)
-            return api_instance
-        except (ModuleNotFoundError, AttributeError) as e:
-            log.error("%s: Error loading API %s: %s",
-                      __class__.__name__, api_name, e)
-            log.debug("%s: %s", __class__.__name__, traceback.format_exc())
-            return None
-
-# Dynamic loading of APIs based on directory structure
-
-
-def load_apis():
-    # TODO Implement dynamic loading based on directory structure instead of a static name list
-    # api_names = [name for name in os.listdir("api") if os.path.isdir(os.path.join("api", name))]
-    api_names = ["ElevenLabsAPI", "ExampleAPI"]
+    """
+    This class is a Singleton factory class that is responsible for loading and managing API instances.
+    """
+    _instance = None
     apis = {}
-    for name in api_names:
-        try:
-            apis[name] = ApiFactory.get_api(name)
-        except Exception as e:
-            log.error("Error loading API %s: %s", name, str(e))
+    api_names = ["ExampleAPI"]
+    active_api_name = api_names[0]
 
-    return apis
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(ApiFactory,cls).__new__(cls)
+        return cls._instance
+
+    def get_api(self, api_name: str):
+        """
+        This method is responsible for loading the API module and creating an instance of the API class.
+        The API class must be named the same as the API module and must be located in the same directory as the module.
+        The loading of the API module is done only once and is done using the importlib module.
+        For subsequent calls the API instance is returned from the internal dictionary.
+
+        :param api_name: The name of the API to load
+        :return: An instance of the API class or None if the API could not be loaded
+        """
+        if self.apis.get(api_name, None) is not None:
+            return self.apis.get(api_name, None)
+        else:
+            try:
+                api_module = importlib.import_module(
+                    f"api.{api_name.lower()}.{api_name.lower()}")
+                log.debug("%s: Imported API module: %s",
+                          __class__.__name__, api_module)
+                api_class = getattr(api_module, api_name)
+                settings_class = getattr(api_module, f"{api_name}Settings")
+                load_widget(os.path.join(os.path.dirname(
+                    api_module.__file__), f"{api_name.lower()}.kv"))
+                # NOTE THIS is the ONLY place where the API instance is created (And should be created)
+                api_instance = api_class(settings_class(settings_class.get_settings_widget()))
+                log.debug("%s: Created API instance: %s", __class__.__name__, api_instance)
+                return api_instance
+            except (ModuleNotFoundError, AttributeError) as e:
+                log.error("%s: Error loading API %s: %s",
+                          __class__.__name__, api_name, e)
+                log.debug("%s: %s", __class__.__name__, traceback.format_exc())
+                return None
+
+    def load_apis(self):
+        """
+        This method is responsible for loading all APIs who are listed in api_names.
+        """
+        # TODO Implement dynamic loading based on directory structure instead of a static name list
+        # api_names = [name for name in os.listdir("api") if os.path.isdir(os.path.join("api", name))]
+
+        for name in self.api_names:
+            try:
+                self.apis[name] = self.get_api(name)
+            except Exception as e:
+                log.error("Error loading API %s: %s", name, str(e))
+
+    def get_default_api(self):
+        """
+        This method returns the default API instance.
+        """
+        return self.apis.get(self.api_names[0], None)
+
+    def get_default_api_name(self):
+        """
+        This method returns the name of the default API.
+        """
+        return self.api_names[0]
+
+    def get_active_api(self):
+        """
+        This method returns the active API instance.
+        """
+        return self.apis.get(self.active_api_name, None)
+
+    def get_active_api_name(self):
+        """
+        This method returns the name of the active API.
+        """
+        return self.active_api_name
+
+    def set_active_api_name(self, api_name: str):
+        """
+        This method sets the active API by name.
+        """
+        self.active_api_name = api_name
+        log.info("Active API set to %s", api_name)
+
+
+api_factory = ApiFactory()
