ba0d6fab38abdc8cad8e6311c90e310cf41564db
philipps <sa22b011@technikum-wien.at>
2024-12-15 14:17:15 +0100

    removed other engines

--- src/screens/main_screen.py
+++ src/screens/main_screen.py
@@ -96,12 +96,6 @@ class MainScreen(MDScreen):
         app = App.get_running_app()
         if hasattr(app, 'api_elevenlabs'):
             app.api_elevenlabs.settings.bind(voice_text=self.update_current_voice)
-        #if hasattr(app, 'api_openai'):
-            #app.api_openai.settings.bind(voice_text=self.update_current_voice)
-        #if hasattr(app, 'api_amazonpolly'):
-            #app.api_amazonpolly.settings.bind(voice_text=self.update_current_voice)
-        #if hasattr(app, 'api_msazure'):
-            #app.api_msazure.settings.bind(voice_text=self.update_current_voice)
 
         # FIXME This is used to keep track of the file manager state (open or closed) but is not currently used
         self.manager_open = False
@@ -118,7 +112,6 @@ class MainScreen(MDScreen):
         self.old_cursor_index = self.ids.text_main.cursor_index()
         Clock.schedule_once(self.set_focus, 0.1)
         self.load_current_voice()
-        app.api_elevenlabs.settings.bind(voice_text=self.update_current_voice)
 
     def amazon_emoji_to_ssml_tag(self, text):
         """
@@ -174,12 +167,6 @@ class MainScreen(MDScreen):
         # Retrieve the default voice for the selected engine from global settings
         if current_engine == "ElevenLabs":
             self.selected_voice = app_instance.global_settings.get_setting("ElevenLabsAPI", "voice", default="Serena")
-        elif current_engine == "OpenAI":
-            self.selected_voice = app_instance.global_settings.get_setting("OpenAIAPI", "voice", default="Echo")
-        elif current_engine == "Amazon Polly":
-            self.selected_voice = app_instance.global_settings.get_setting("AmazonPollyAPI", "voice", default="Vicki")
-        elif current_engine == "Microsoft Azure":
-            self.selected_voice = app_instance.global_settings.get_setting("MSAzureAPI", "voice", default="Ingrid (de-AT)")
         else:
             self.selected_voice = ""  # Fallback if no engine is set
 
@@ -354,9 +341,6 @@ class MainScreen(MDScreen):
         app = App.get_running_app()
         available_engines = {
             "ElevenLabs": app.api_elevenlabs,
-            "OpenAI": app.api_openai,
-            "Amazon Polly": app.api_amazonpolly,
-            "Microsoft Azure": app.api_msazure
         }
 
         # Prepare menu items for each available TTS engine
@@ -385,12 +369,6 @@ class MainScreen(MDScreen):
         # Retrieve the current voice for the selected engine
         if engine_name == "ElevenLabs":
             self.selected_voice = app.api_elevenlabs.settings.voice_text
-        elif engine_name == "OpenAI":
-            self.selected_voice = app.api_openai.settings.voice_text
-        elif engine_name == "Amazon Polly":
-            self.selected_voice = app.api_amazonpolly.settings.voice_text
-        elif engine_name == "Microsoft Azure":
-            self.selected_voice = app.api_msazure.settings.voice_text
         else:
             self.selected_voice = "N/A"
 
@@ -413,12 +391,6 @@ class MainScreen(MDScreen):
         # Determine which API to use based on the current engine
         if current_engine == "ElevenLabs":
             api = app.api_elevenlabs
-        elif current_engine == "OpenAI":
-            api = app.api_openai
-        elif current_engine == "Amazon Polly":
-            api = app.api_amazonpolly
-        elif current_engine == "Microsoft Azure":
-            api = app.api_msazure
         else:
             log.error("Unknown TTS engine selected.")
             return
@@ -454,12 +426,6 @@ class MainScreen(MDScreen):
         # Update the selected voice in the appropriate API's settings
         if current_engine == "ElevenLabs":
             app.api_elevenlabs.set_voice(voice_name)
-        elif current_engine == "OpenAI":
-            app.api_openai.set_voice(voice_name)
-        elif current_engine == "Amazon Polly":
-            app.api_amazonpolly.set_voice(voice_name)
-        elif current_engine == "Microsoft Azure":
-            app.api_msazure.set_voice(voice_name)
 
         # Update displayed selected voice
         self.selected_voice = voice_name
@@ -471,12 +437,6 @@ class MainScreen(MDScreen):
         # Check the selected engine and call the respective synthesis method
         if current_engine == "ElevenLabs":
             self.on_synthesize_elevenlabs()
-        elif current_engine == "OpenAI":
-            self.on_synthesize_openai()
-        elif current_engine == "Amazon Polly":
-            self.on_synthesize_amazonpolly()
-        elif current_engine == "Microsoft Azure":
-            self.on_synthesize_msazure()
         else:
             log.error("No valid TTS engine selected.")
 
@@ -503,60 +463,6 @@ class MainScreen(MDScreen):
                                    size_hint=(None, None), size=(400, 400))
         popup_window.open()
 
-    def on_synthesize_openai(self):
-        api = App.get_running_app().api_openai
-        tmp_dir = App.get_running_app().global_settings.get_tmp_dir()
-        file_path = os.path.join(tmp_dir, 'openai_output.mp3')
-        log.info(f"Using file_path={file_path}")
-
-        if api:
-            try:
-                api.synthesize(self.ids.text_main.text, file_path)
-            except Exception as e:
-                msg = "Error during synthesis"
-                log.error("%s: %s: %s", self.__class__.__name__, msg, e)
-                self.ids.label_status.text = msg
-        popup_window = CustomPopup(content_text="Text synthesized to an audio file.",
-                                   size_hint=(None, None), size=(400, 400))
-        popup_window.open()
-
-    def on_synthesize_amazonpolly(self):
-        api = App.get_running_app().api_amazonpolly
-        tmp_dir = App.get_running_app().global_settings.get_tmp_dir()
-        file_path = os.path.join(tmp_dir, 'amazon_polly_output.mp3')
-        log.info(f"Using file_path={file_path}")
-
-        if api:
-            try:
-                processed_text = self.amazon_emoji_to_ssml_tag(self.ids.text_main.text)
-
-                # Pass text_type to the Amazon Polly API for synthesis
-                api.synthesize(processed_text, file_path, text_type=self.text_type)
-            except Exception as e:
-                msg = "Error during synthesis"
-                log.error("%s: %s: %s", self.__class__.__name__, msg, e)
-                self.ids.label_status.text = msg
-        popup_window = CustomPopup(content_text="Text synthesized to an audio file.",
-                                   size_hint=(None, None), size=(400, 400))
-        popup_window.open()
-
-    def on_synthesize_msazure(self):
-        api = App.get_running_app().api_msazure
-        tmp_dir = App.get_running_app().global_settings.get_tmp_dir()
-        file_path = os.path.join(tmp_dir, 'azure_output.wav')
-        log.info(f"Using file_path={file_path}")
-
-        if api:
-            try:
-                api.synthesize(self.ids.text_main.text, file_path, ssml_conversion_func=self.msazure_emoji_to_ssml_tag)
-            except Exception as e:
-                msg = "Error during synthesis"
-                log.error("%s: %s: %s", self.__class__.__name__, msg, e)
-                self.ids.label_status.text = msg
-        popup_window = CustomPopup(content_text="Text synthesized to an audio file.",
-                                   size_hint=(None, None), size=(400, 400))
-        popup_window.open()
-
     def play(self, audio_dir):
         try:
             mixer.init()
@@ -576,12 +482,6 @@ class MainScreen(MDScreen):
         # Check the selected engine and call the respective play function
         if current_engine == "ElevenLabs":
             audio_dir = os.path.join(tmp_dir, 'elevenlab_output.wav')
-        elif current_engine == "OpenAI":
-            audio_dir = os.path.join(tmp_dir, 'openai_output.mp3')
-        elif current_engine == "Amazon Polly":
-            audio_dir = os.path.join(tmp_dir, 'amazon_polly_output.mp3')
-        elif current_engine == "Microsoft Azure":
-            audio_dir = os.path.join(tmp_dir, 'azure_output.wav')
         else:
             log.error("No valid TTS engine selected.")
 
