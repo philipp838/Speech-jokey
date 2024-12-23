1cd413d3ee74d2bf2d8a2666acaac6a51794a6dd
Martin Deinhofer <deinhofer.m@gmail.com>
2024-12-23 09:21:57 +0100

    Use display name as stored value in global_settings instead of internal_name

--- src/api/gttsapi/gttsapi.py
+++ src/api/gttsapi/gttsapi.py
@@ -147,9 +147,9 @@ class GttsAPI(BaseApi):
             (v for v in self.voices if v["display_name"] == display_name), None
         )
         if selected_voice:
-            self.settings.voice_text = selected_voice["internal_name"]
-            self.settings.save_settings()
             log.info(f"Voice set to: {display_name}")
+            self.settings.voice_text = selected_voice["display_name"]
+            self.settings.save_settings()
         else:
             log.error("Voice not found for display name: %s", display_name)
 
@@ -181,6 +181,6 @@ class GttsAPI(BaseApi):
 
     def __get_selected_voice(self):
         selected_voice = next(
-            (v for v in self.voices if v['internal_name'] == self.settings.voice_text), None
+            (v for v in self.voices if v['display_name'] == self.settings.voice_text), None
         )
         return selected_voice
