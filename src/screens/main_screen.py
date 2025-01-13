f6ec6b89511c2bffc8273add9d51db42f7bba42e
philipps <sa22b011@technikum-wien.at>
2025-01-14 00:29:14 +0100

    Increased default text size to 24

--- src/screens/main_screen.py
+++ src/screens/main_screen.py
@@ -84,6 +84,7 @@ class MainScreen(MDScreen):
     def __init__(self, title: str, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
         self.title = title
+        self.ids.text_main.font_size = 24
         self.original_font_size = self.ids.text_main.font_size
         # Initialize the current TTS engine text
         self.update_current_engine_text()
